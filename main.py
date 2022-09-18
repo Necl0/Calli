4480a54a1df58e32aa169c2c64e306842734447a
Josiah <63004866+Necl0@users.noreply.github.com>
2022-09-17 23:19:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,11 @@ import torchvision.transforms as transforms
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Hyperpaarameters
-num_epochs = 5
+# Hyper parameters
+num_epochs = 20
 num_classes = 10
 batch_size = 32
 learning_rate = 0.001
-
 
 # load KMNIST
 train_data = torchvision.datasets.KMNIST(root='../../data/',
@@ -23,17 +22,18 @@ train_data = torchvision.datasets.KMNIST(root='../../data/',
                                          download=True)
 
 test_data = torchvision.datasets.KMNIST(root='../../data/',
-                                          train=False, 
-                                          transform=transforms.ToTensor())
+                                        train=False,
+                                        transform=transforms.ToTensor())
 
 # Data Loader
 train_loader = DataLoader(dataset=train_data,
-                                           batch_size=batch_size, 
-                                           shuffle=True)
+                          batch_size=batch_size,
+                          shuffle=True)
 
 test_loader = DataLoader(dataset=test_data,
-                                          batch_size=batch_size, 
-                                          shuffle=False)
+                         batch_size=batch_size,
+                         shuffle=False)
+
 
 # Convolutional neural network (two convolutional layers)
 class ConvNet(nn.Module):
@@ -49,14 +49,15 @@ class ConvNet(nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
-        self.fc = nn.Linear(7*7*32, num_classes)
-        
+        self.fc = nn.Linear(7 * 7 * 32, num_classes)
+
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
         out = out.reshape(out.size(0), -1)
         out = self.fc(out)
         return out
+
 
 model = ConvNet(num_classes).to(device)
 
@@ -70,33 +71,36 @@ for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
         images = images.to(device)
         labels = labels.to(device)
-        
+
         # Forward pass
         outputs = model(images)
         loss = criterion(outputs, labels)
-        
+
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
-        if (i+1) % 100 == 0:
-            print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{total_step}], Loss: {loss.item()}')
-            
+
+        if (i + 1) % 100 == 0:
+            print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{total_step}], Loss: {loss.item()}')
+
 # Test the model
 model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
+
 with torch.no_grad():
     correct = 0
     total = 0
+
     for images, labels in test_loader:
         images = images.to(device)
         labels = labels.to(device)
         outputs = model(images)
+
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
     print(f'Test Accuracy of the model on the 10000 test images: {100 * correct / total}')
-    
+
 with open('model_pkl', 'wb') as files:
     pickle.dump(model, files)
