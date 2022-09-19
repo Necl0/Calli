7c1a049ca559cba7762df43a8cdd85ad3b53f795
Josiah <63004866+Necl0@users.noreply.github.com>
2022-09-19 15:41:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import torch
 import torch.nn as nn
 import torchvision
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
-
 import numpy as np
 import pickle
 
@@ -25,13 +24,13 @@ class Hyperparameters(BaseModel):
     def power_of_two(cls, v: int):
         assert (v & (v-1) == 0) and v != 0, 'Batch size should be a power of two.'
         return v
-   
+
 try:
     m1 = Hyperparameters()
 except ValidationError as e:
     print(e)
 
-    
+
 # load KMNIST
 train_data = torchvision.datasets.KMNIST(root='../../data/',
                                          train=True,
